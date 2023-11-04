e786e15312feb454b03b0124de89fe9c02c519b6
Aaron Jones <ajbowhunterii@gmail.com>
2023-11-04 15:56:21 -0600

    Trying to fix idk

--- blackjackModel.py
+++ blackjackModel.py
@@ -26,36 +26,36 @@ def simulate_player(player_hand, dealer_upcard, deck):
         else:
             return -1  # Player loses
 
-    # Expected value of hitting
-    hit_outcomes = [simulate_player(player_hand + [deck.draw_card()], dealer_upcard, deck) for _ in range(1000)]
-    hit_ev = sum(hit_outcomes) / 1000.0
-
-    # Expected value of standing
-    stand_outcomes = [simulate_dealer(dealer_upcard, deck) for _ in range(1000)]
-    stand_wins = sum(1 for outcome in stand_outcomes if outcome < sum(player_hand) or outcome > 21)
-    stand_pushes = sum(1 for outcome in stand_outcomes if outcome == sum(player_hand))
-    stand_losses = 1000 - stand_wins - stand_pushes
-    stand_ev = (stand_wins - stand_losses) / 1000.0
-
-    # If the player's hand is less than 17, return the expected value of hitting
+    # If the player's hand is less than 17, simulate the expected value of hitting
     if sum(player_hand) < 17:
+        hit_outcomes = [simulate_player(player_hand + [deck.draw_card()], dealer_upcard, deck) for _ in range(1000)]
+        hit_ev = sum(hit_outcomes) / 1000.0
         return hit_ev
 
-    # Otherwise, return the expected values as a tuple
-    return hit_ev, stand_ev
+    # This point should not be reached because the function should return before here
+    raise ValueError("Reached an unexpected point in simulate_player")
+
+def make_decision(player_hand, dealer_upcard, deck):
+    # If the player's hand is less than 17, simulate hitting and standing to decide
+    if sum(player_hand) < 17:
+        hit_ev = simulate_player(player_hand + [deck.draw_card()], dealer_upcard, deck)
+        # Simulate standing
+        dealer_total = simulate_dealer(dealer_upcard, deck)
+        if dealer_total > 21 or sum(player_hand) > dealer_total:
+            stand_ev = 1  # Player wins
+        elif sum(player_hand) == dealer_total:
+            stand_ev = 0  # Push
+        else:
+            stand_ev = -1  # Player loses
+        return 'hit' if hit_ev > stand_ev else 'stand'
+    else:
+        # If the player's hand is 17 or more, always stand
+        return 'stand'
 
 if __name__ == "__main__":
     deck = Deck()
     for dealer_upcard in range(1, 11):
         for player_initial_total in range(4, 21):  # Assuming player won't hit below 4
             player_hand = [player_initial_total - 1, 1]  # Simplified initial hand
-            result = simulate_player(player_hand, dealer_upcard, deck)
-            # If the result is a tuple, unpack it
-            if isinstance(result, tuple):
-                hit_ev, stand_ev = result
-                optimal_strategy = 'hit' if hit_ev > stand_ev else 'stand'
-            else:
-                # If the result is not a tuple, it's the expected value of hitting
-                hit_ev = result
-                optimal_strategy = 'hit' if hit_ev > 0 else 'stand'
+            optimal_strategy = make_decision(player_hand, dealer_upcard, deck)
             print(f"Dealer shows: {dealer_upcard}, Player has: {player_initial_total}, Optimal strategy: {optimal_strategy}")
